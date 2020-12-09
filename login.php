fd71f4c84a4f3ab3917f62da64e178f4ca447b42
GAGAN CHAHAL <gchahal35@academic.rrc.ca>
2020-12-09 15:13:19 -0600

    fifth_commit

--- login.php
+++ login.php
@@ -1,47 +1,37 @@
 <?php
 
-// //Initialize the session.
 session_start();
-
 require('connect.php');
 error_reporting(E_ALL ^ E_NOTICE);
 
 $error = [];
 
-if(isset($_POST['username']) AND isset($_POST['password'])){
-
+if(isset($_POST['username']) AND isset($_POST['password']))
+{
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
-	//$spassword = password_hash($password, PASSWORD_BCRYPT);
 
 	$query = "SELECT password FROM users WHERE username = '$username' ";
-	
-
-	// if($count == 1){
-	// 	echo "You are now logged in";
-	// }
 	$prepare = $db->prepare($query);
-
  	if($prepare->execute())
-        {
-        	$pass = $prepare->fetch();
-        	$secret = $pass["password"];
-        	
-            if($prepare->rowCount()==1 && password_verify($password, $secret))
-           	{
-           		$_SESSION['user'] = $username;
-                 $var = "You are now logged in";
-                 header('location: homepage.php');           	
-             }
-           	
-           	else{
-				$var = "Invalid Username/password combination.";
-		    }
+    {
+    	$pass = $prepare->fetch();
+    	$secret = $pass["password"];
+    	
+        if($prepare->rowCount()==1 && password_verify($password, $secret))
+       	{
+       		$_SESSION['user'] = $username;
+            $var = "You are now logged in";
+            header('location: homepage.php');           	
+         }
+       	
+       	else
+       	{
+			$var = "Invalid Username/password combination.";
+	    }
 
-        }
     }
-    
-
+}
 
 ?>
 
@@ -52,7 +42,6 @@ if(isset($_POST['username']) AND isset($_POST['password'])){
 	<link rel="stylesheet" type="text/css" href="loginforms.css">
 </head>
 <body id="yuhu">
-
 	<div class="login">
 		<form id="event" method="post" action="login.php">
 			<h1>Login</h1>
@@ -77,9 +66,7 @@ if(isset($_POST['username']) AND isset($_POST['password'])){
 					</li>
 				</ol>
 			</fieldset>
-  	</form>
-
+  		</form>
 	</div>
-
 </body>
 </html>
