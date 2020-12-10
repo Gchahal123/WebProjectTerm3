5b0f2dfe8b2f0a3eae0589b8a3fb741866a8907f
GAGAN CHAHAL <gchahal35@academic.rrc.ca>
2020-12-09 23:29:40 -0600

    tenth_commit

--- edit.php
+++ edit.php
@@ -1,6 +1,12 @@
 <?php
     
-    require 'connect.php';
+    session_start();
+    require ('connect.php');
+
+    if(!isset($_SESSION['user']))
+    {
+        header('location: login.php');
+    }
 
     $query = "SELECT * FROM category WHERE categoryid = {$_GET['categoryid']}";
     $state = $db->prepare($query);
@@ -9,10 +15,10 @@
     $id = $_GET['categoryid'];
     $message = " ";
 
-    if (isset($_POST['submit']))
+    if (isset($_POST['update']))
     {
-        $categoryname = $_POST['categoryname'];
-        $old_image = $_POST['old_image'];
+        $categoryname = filter_input(INPUT_POST, "categoryname", FILTER_SANITIZE_STRING);
+        $old_image = filter_input(INPUT_POST, "old_image", FILTER_SANITIZE_STRING);
         $new_image = $_FILES['image']['name'];
         $image_stored_location = "images/".$new_image;
         $image_tem_location = $_FILES['image']['tmp_name'];
@@ -24,55 +30,49 @@
         else
         {
             $image_stored_location = $old_image;
-            
             $query = "UPDATE category SET categoryname = :categoryname WHERE categoryid = :categoryid";
 
             $statement = $db->prepare($query);
-		    $statement->bindValue(':categoryname', $categoryname);  
-		    $statement->bindValue(':categoryid', $id, PDO::PARAM_INT);
-		    $statement->execute();
-		    header('Location:homepage.php?categoryid='.$id);
+            $statement->bindValue(':categoryname', $categoryname);  
+            $statement->bindValue(':categoryid', $id, PDO::PARAM_INT);
+            $statement->execute();
+            header('Location:homepage.php?categoryid='.$id);
         }
 
         if(!(file_exists($image_stored_location)))
         {
             $query = "UPDATE category SET categoryname = :categoryname, images = :images WHERE categoryid = :categoryid";  
 
-        $statement = $db->prepare($query);
-        $statement->bindValue(':categoryname', $categoryname);
-        $statement->bindValue(':images', $image_stored_location);   
-	    $statement->bindValue(':categoryid', $id, PDO::PARAM_INT);
-	    
-        $statement->execute();
-        move_uploaded_file($image_tem_location,$image_stored_location);
+            $statement = $db->prepare($query);
+            $statement->bindValue(':categoryname', $categoryname);
+            $statement->bindValue(':images', $image_stored_location);   
+            $statement->bindValue(':categoryid', $id, PDO::PARAM_INT);
+            
+            $statement->execute();
+            move_uploaded_file($image_tem_location,$image_stored_location);
         }
-
         else
         {
-            
-           $query = "UPDATE category SET categoryname = :categoryname, images = :images WHERE categoryid = :categoryid"; 
-	        $statement = $db->prepare($query);
-	        $statement->bindValue(':categoryname', $categoryname);
-	        $statement->bindValue(':images', $image_stored_location);
-		    $statement->bindValue(':categoryid', $id, PDO::PARAM_INT);
-
+            $query = "UPDATE category SET categoryname = :categoryname, images = :images WHERE categoryid = :categoryid"; 
+            $statement = $db->prepare($query);
+            $statement->bindValue(':categoryname', $categoryname);
+            $statement->bindValue(':images', $image_stored_location);
+            $statement->bindValue(':categoryid', $id, PDO::PARAM_INT);
         
            if($statement->execute())
            {
-             	if($_FILES['image']['name'] != '')
-             	{
-                	move_uploaded_file($image_tem_location,$image_stored_location);
+             if($_FILES['image']['name'] != '')
+             {
+                move_uploaded_file($image_tem_location,$image_stored_location);
+             }
 
-             	}
-
-             	$message = " COngrats! Your data is updated.";
+             $message = " Congrats! Your data is updated.";
            }
 
            else
            {
              $message = "The file could not updated.";
            }
-
         }
     }
 
@@ -82,25 +82,18 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-	<title>Edit php</title>
-	<link rel="stylesheet" type="text/css" href="edit.css">
+    <title>Edit php</title>
+    <!-- <link rel="stylesheet" type="text/css" href="edit.css"> -->
 </head>
 <body>
-	<form method="post">
-
-		<input type="text" name="categoryname" id="categoryname" value="<?= $post[0]['categoryname']?>"><br>
-
-		<img src="<?= $post[0]['image'];?>" width="200">
+    <form method="post">
+        <input type="text" name="categoryname" id="categoryname" value="<?= $post[0]['categoryname']?>"><br>
+        <img src="<?= $post[0]['image'];?>" width="200">
         
-		<input type="hidden" name="old_image" id="image" value="<?= $post[0]['image'] ?>">
-
+        <label>Upload new image: </label><br>
+        <input type="text" name="old_image" id="image" value="<?= $post[0]['image'] ?>">
         <input type="file" name="image">
-
-		<button type="submit" name="submit" value="submit">Update</button>
-
-		 
-
-	</form>
-
+        <button type="submit" name="update" value="submit">Update</button>
+    </form>
 </body>
 </html>
